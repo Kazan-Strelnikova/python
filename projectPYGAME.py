61ba2d7ee315e8900a8cbe0a52bdeb4cc14e9c96
Kazan-Strelnikova <59692605+Kazan-Strelnikova@users.noreply.github.com>
2020-01-15 22:27:32 +0300

    Add files via upload

--- projectPYGAME.py
+++ projectPYGAME.py
@@ -11,19 +11,25 @@ pygame.init()
 size = 539, 539
 screen = pygame.display.set_mode(size)
 image = pygame.Surface([100, 100])
-cell = 0
 UP = 'up'
 DOWN = 'down'
 LEFT = 'left'
 RIGHT = 'right'
 
 HEAD = 0
-cell = size[0] // 29#кол-во клеточек
+cell = [0, 0]
 
+cameraX = 0
+cameraY = 0
+levelnow = 1
 def load_image(name):
     fullname = os.path.join(name)
     image = pygame.image.load(fullname).convert_alpha()
     return image
+
+def draw_image(name, pos):
+    surf = load_image(name)
+    screen.blit(surf, (pos[0] + cameraX, pos[1] + cameraY))
 
 
 def terminate():
@@ -31,19 +37,23 @@ def terminate():
     sys.exit()
 
 
-
+appl = 0
 def runGame():
-    global headpos
+    global headpos, appl
+    appl = 0
     screen.fill((0, 100, 0))
     all_sprites.draw(screen)
     # Set a random start point.
-    startx = random.randint(5, cell - 6)
-    starty = random.randint(5, cell - 6)
+    startx = random.randint(5, cell[0] - 6)
+    starty = random.randint(5, cell[1] - 6)
     snake = [{'x': startx, 'y': starty}, {'x': startx - 1, 'y': starty}, {'x': startx - 2, 'y': starty}]
     direction = RIGHT
 
     # Start the apple in a random place.
     apple = getRandomLocation()
+    if level[apple['y']][apple['x']] == '#':
+            apple = getRandomLocation()
+
 
     while True:  # main game loop
         for event in pygame.event.get():  # event handling loop
@@ -62,7 +72,8 @@ def runGame():
                     terminate()
 
         # check if the worm has hit itself or the edge
-        if snake[HEAD]['x'] == -1 or snake[HEAD]['x'] == cell or snake[HEAD]['y'] == -1 or snake[HEAD]['y'] == cell:
+        if snake[HEAD]['x'] == -1 or snake[HEAD]['x'] == cell[0] \
+                or snake[HEAD]['y'] == -1 or snake[HEAD]['y'] == cell[1]:
             gameover()  # game over
         for wormBody in snake[1:]:
             if wormBody['x'] == snake[HEAD]['x'] and wormBody['y'] == snake[HEAD]['y']:
@@ -71,9 +82,16 @@ def runGame():
         # check if worm has eaten an apply
         if snake[HEAD]['x'] == apple['x'] and snake[HEAD]['y'] == apple['y']:
             # don't remove worm's tail segment
-            apple = getRandomLocation()  # set a new apple somewhere
+            apple = getRandomLocation()
+            if level[apple['y']][apple['x']] == '#':
+                apple = getRandomLocation()
+            # set a new apple somewhere
+            appl += 1
         else:
             del snake[-1]  # remove worm's tail segment
+
+        if level[snake[HEAD]['y']][snake[HEAD]['x']] == '#':
+            gameover()
 
         # move the worm by adding a segment in the direction it is moving
         if direction == UP:
@@ -87,6 +105,13 @@ def runGame():
         snake.insert(0, newHead)
         headpos = newHead
 
+        global cameraX, cameraY
+        cameraX = -(headpos['x'] - 8.5) * tile_width
+        cameraY = -(headpos['y'] - 8.5) * tile_height
+
+        if appl == 3:
+            nextLevel()
+
         drawSnake(snake, direction)
         drawApple(apple)
         pygame.display.update()
@@ -94,7 +119,7 @@ def runGame():
 
 
 def getRandomLocation():
-    return {'x': random.randint(0, cell - 1), 'y': random.randint(0, cell - 1)}
+    return {'x': random.randint(0, cell[0] - 1), 'y': random.randint(0, cell[1] - 1)}
 
 
 def start_screen():
@@ -119,7 +144,7 @@ def start_screen():
             if event.type == pygame.QUIT:
                 terminate()
             elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
-                return  # начинаем игру
+                return
         pygame.display.flip()
         clock.tick(FPS)
 
@@ -129,7 +154,7 @@ def gameover():
     font = pygame.font.Font(None, 200)
     text_coord = 50
     for line in intro_text:
-        string_rendered = font.render(line, 1, pygame.Color(247, 121, 167))
+        string_rendered = font.render(line, 1, (247, 121, 167))
         intro_rect = string_rendered.get_rect()
         text_coord += 10
         intro_rect.top = text_coord
@@ -142,19 +167,17 @@ def gameover():
             if event.type == pygame.QUIT:
                 terminate()
             elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
-                runGame()  # начинаем игру
+                runGame()
         pygame.display.flip()
         clock.tick(FPS)
 
 
 def load_level(filename):
-    global cell
     with open(filename, 'r') as mapFile:
         level_map = [line.strip() for line in mapFile]
-    # и подсчитываем максимальную длину
     max_width = max(map(len, level_map))
-    #cell = max_width если поле больше экрана
-    # дополняем каждую строку пустыми клетками ('.')
+    cell[0] = max_width
+    cell[1] = len(level_map) - 1
     return list(map(lambda x: x.ljust(max_width, '.'), level_map))
 
 
@@ -184,35 +207,86 @@ def generate_level(level):
 
 
 def drawSnake(snake, direction):
-    all_sprites.draw(screen)
+    screen.fill((0, 0, 0))
+    sprites_surf = pygame.Surface(size)
+    all_sprites.draw(sprites_surf)
+    screen.blit(sprites_surf, (cameraX, cameraY))
     for coord in snake:
         x = coord['x'] * tile_width
         y = coord['y'] * tile_height
         if coord == snake[0] and direction == UP:
-            image = load_image("headu.png")
-            screen.blit(image, (x, y))
+            image = "headu.png"
         elif coord == snake[0] and direction == DOWN:
-            image = load_image("headd.png")
-            screen.blit(image, (x, y))
+            image = "headd.png"
         elif coord == snake[0] and direction == LEFT:
-            image = load_image("headl.png")
-            screen.blit(image, (x, y))
+            image = "headl.png"
         elif coord == snake[0] and direction == RIGHT:
-            image = load_image("headr.png")
-            screen.blit(image, (x, y))
+            image = "headr.png"
         else:
-            image = load_image("body.png")
-            screen.blit(image, (x, y))
+            image = "body.png"
+        draw_image(image, (x, y))
+
+def nextLevel():
+    global lev, level, levelnow
+    if levelnow == 3:
+        win()
+        pass
+    intro_text = [" NEXT ", ' LEVEL ']
+    font = pygame.font.Font(None, 200)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color(247, 121, 167))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 30
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+                if levelnow == 1:
+                    lev = 'map2.txt'
+                elif levelnow == 2:
+                    lev = 'map3.txt'
+                level = load_level(lev)
+                generate_level(load_level(lev))
+                levelnow += 1
+                runGame()  # начинаем игру
+        pygame.display.flip()
+        clock.tick(FPS)
 
 
 def drawApple(apple):
     x = apple['x'] * tile_width
     y = apple['y'] * tile_height
-    image = load_image("apple.png")
-    screen.blit(image, (x, y))
+    draw_image("apple.png", (x, y))
 
 
+def win():
+    intro_text = ["  YOU ", ' WIN!!! ']
+    font = pygame.font.Font(None, 200)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, (247, 121, 167))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 30
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
 
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+                runGame()
+        pygame.display.flip()
+        clock.tick(FPS)
 level = load_level('map1.txt')
 level_x, level_y = generate_level(load_level('map1.txt'))
 
